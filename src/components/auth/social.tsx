c92b2e2a49e0e38daf8c04b114a7a7e16bd7dd4f
Gabriel Souza <glopes@techsocial.com.br>
2024-01-31 22:02:34 -0300

    :sparkles: Install react-icons and create socials component

--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -1,5 +1,30 @@
 import React from 'react'
 
+import { FcGoogle } from 'react-icons/fc'
+import { FaFacebook } from 'react-icons/fa'
+
+import { Button } from '@/components/ui/button'
+
 export default function Social() {
-    return <div className="flex items-center w-full gap-x-2"></div>
+    return (
+        <div className="flex items-center w-full gap-x-2">
+            <Button
+                type="button"
+                className="w-full flex justify-evenly"
+                variant="outline"
+            >
+                <FcGoogle className="w-6 h-6" />
+                <span>Google</span>
+            </Button>
+
+            <Button
+                type="button"
+                className="w-full flex justify-evenly"
+                variant="outline"
+            >
+                <FaFacebook className="w-6 h-6" />
+                <span>Facebook</span>
+            </Button>
+        </div>
+    )
 }
